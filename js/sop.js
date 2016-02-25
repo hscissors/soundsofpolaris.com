19cd739a9337c767ed68d7849e46e76d98b3f8d8
Harold <harold.scissors@teamsnap.com>
2016-02-25 12:36:59 -0800

    init commit

---
+++ js/sop.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	$("#header-wrapper .header-title").fitText(.73, { minFontSize: '30px', maxFontSize: '140px' });
+	$("#header-wrapper .header-desc").fitText(2, { minFontSize: '18px', maxFontSize: '35px' });
+
+});
+
